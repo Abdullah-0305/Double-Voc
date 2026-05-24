785d08d05da7d0a515907c4e4bf89f6ace63ad55
Abdullah-0305 <tagariabdullah@gmail.com>
2026-05-24 16:20:16 +0200

    Ajout des dépendances pour compil

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+require('opusscript');
+
 const { Client, GatewayIntentBits, Events, ChannelType, PermissionFlagsBits } = require('discord.js');
 const prism = require('prism-media');
 const { Mixer } = require('audio-mixer');
@@ -196,14 +198,3 @@ botSpeakers.forEach((bot, index) => {
     bot.once(Events.ClientReady, () => console.log(`🟢 Speaker ${index + 1} prêt !`));
     bot.login(tokensSpeakers[index]);
 });
-
-// --- FAUX SERVEUR WEB POUR RENDER ---
-const http = require('http');
-const port = process.env.PORT || 3000;
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Le bot Discord est en ligne et fonctionne parfaitement !\n');
-}).listen(port, () => {
-    console.log(`🌐 Faux serveur web démarré sur le port ${port} pour Render !`);
-});
-// -----------------------------------------------
